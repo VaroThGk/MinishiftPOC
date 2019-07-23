8c5ece8e7ceb0571d3a337a1045a62f141ae4677
Alvaro Nunez Garita <alvaro_nunez@hotmail.com>
2019-07-23 10:55:09 -0600

    Added Client Class

---
+++ src/main/java/com/example/models/Client.java
@@ -0,0 +1,34 @@
+package com.example.models;
+
+public class Client {
+
+	private String message;
+	private String name;
+	private long id;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+}
